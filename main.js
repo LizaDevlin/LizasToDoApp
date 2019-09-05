8678df154688a4587f37914ba5e1e0ae59c0392b
Liza <lisa.yaniuk@gmail.com>
2019-09-05 16:13:55 +0300

    created a base

---
+++ main.js
@@ -0,0 +1,52 @@
+calss ToDoApp {
+    execute()
+
+    dispose()
+}
+
+class Task{
+    constructor (title){
+        this.title = title;
+        isDone = false;
+    }
+    done(){
+        isDone = true;
+    }
+}
+
+class TaskManager {
+    createTask()
+
+    removeTask()
+
+    updateTask()
+}
+
+clss Store {
+    createTask(title)
+
+    removeTask()
+
+    getTasks()
+
+    getTask()
+
+    updateTask()
+}
+
+class StoreJS {
+
+}
+
+class StoreLS {
+
+}
+
+class Render {
+    display()
+
+    redrerTask()
+
+    updateTask()
+}
+
